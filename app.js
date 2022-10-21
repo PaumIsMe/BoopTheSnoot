88e08ccfbe013c5acc8bd543e5a01a9ad8445022
PaumIsMe <theapologeticweasels@gmail.com>
2022-10-21 11:46:15 -0400

    Use rendering

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const { readFile } = require('fs').promises;
 
 const app = express();
 
-app.get('/', async (request, response) => {
-    response.send( await readFile('./home.html', 'utf8'))
+app.get('/', function(req, res){
+    res.render('index.html', {
+          title: 'My Site',
+      nav: ['Home','About','Contact'] 
+    });
 });
 
 app.listen(process.env.PORT || 3000, () => console.log('App available on http://localhost:3000'))
