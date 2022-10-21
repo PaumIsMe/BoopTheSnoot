39d8473aae8d5bdf351aba11624dfe6d19b925aa
PaumIsMe <theapologeticweasels@gmail.com>
2022-10-21 12:29:01 -0400

    refactor fs

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public', 'css')));
+
+var port = process.env.PORT || 3000
+app.listen(port, () => console.log('App available on http://localhost:', port))
